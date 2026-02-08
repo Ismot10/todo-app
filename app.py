bc6e565a833a76305dd73cab853eebeb57ba740e
Ismot10 <ismotaraprova@gmail.com>
2026-02-09 04:48:37 +0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+# Initialize Flask app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize database
+db = SQLAlchemy(app)
+
+# Create a form for adding tasks
+class TaskForm(FlaskForm):
+    task = StringField('Task', validators=[DataRequired()])
+    submit = SubmitField('Add Task')
+
+# Create a database model for tasks
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+# Home route - Display To-Do List
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = TaskForm()
+    tasks = Task.query.all()
+    if form.validate_on_submit():
+        new_task = Task(content=form.task.data)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, tasks=tasks)
+
+# Delete task route
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    task_to_delete = Task.query.get_or_404(task_id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# Start the app
+if __name__ == "__main__":
+    db.create_all()  # Create tables in the database
+    app.run(debug=True)
