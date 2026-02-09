f691a74dde1551b4d44bb8c7f02bd1b2999b1891
Ismot10 <ismotaraprova@gmail.com>
2026-02-09 16:38:01 +0600

    Your message

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 import os
 
-=======
->>>>>>> c2a363565545af7a2d65ece1739857f971f7c6c2
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -11,19 +9,18 @@ from wtforms.validators import DataRequired
 
 # Initialize Flask app
 app = Flask(__name__)
-<<<<<<< HEAD
+ 
 app.config['SECRET_KEY'] = '90c8005869ae1d043dc5ccfd274d3436'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:/todo-app/instance/todo.db'
 
-=======
-app.config['SECRET_KEY'] = 'your_secret_key_here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/todo.db'
->>>>>>> c2a363565545af7a2d65ece1739857f971f7c6c2
+ 
+ 
+ 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize database
 db = SQLAlchemy(app)
-<<<<<<< HEAD
+ 
  
 
 # Get the absolute path to the todo.db file inside the instance folder
@@ -32,8 +29,8 @@ db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', '
 # Update the database URI with the absolute path
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
-=======
->>>>>>> c2a363565545af7a2d65ece1739857f971f7c6c2
+ 
+ 
 
 # Create a form for adding tasks
 class TaskForm(FlaskForm):
@@ -65,16 +62,16 @@ def delete(task_id):
     db.session.commit()
     return redirect(url_for('index'))
 
-<<<<<<< HEAD
+ 
 # Create tables and run the app within an application context
 if __name__ == '__main__':
     with app.app_context():  # Ensure the app context is active
         db.create_all()  # Create tables in the database
     app.run(debug=True)
+ 
 
-=======
 # Start the app
 if __name__ == "__main__":
     db.create_all()  # Create tables in the database
     app.run(debug=True)
->>>>>>> c2a363565545af7a2d65ece1739857f971f7c6c2
+ 
